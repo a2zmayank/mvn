cff514dcb7d530698882f89dee56f7cc8eecbbf4
Mayank Gautam <mg.gautam86@gmail.com>
2023-02-16 18:04:51 +0530

    Hello Update

--- PublicPages/src/test/java/CommLoanPublicPages/testPerposs.java
+++ PublicPages/src/test/java/CommLoanPublicPages/testPerposs.java
@@ -4,6 +4,7 @@ public class testPerposs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hellow");
+		System.out.print("Hello");
+		System.out.print("Hello");
 	}
 }
