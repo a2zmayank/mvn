e48c14cc7648f6cbd85eb49584e3152c899b5c16
Mayank Gautam <mg.gautam86@gmail.com>
2023-02-16 18:42:12 +0530

    Done

---
+++ BorrrowerPortal/src/test/java/CommLoan/BorrrowerPortal/PushCodeGIT.java
@@ -0,0 +1,12 @@
+package CommLoan.BorrrowerPortal;
+
+public class PushCodeGIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hi Kapil");
+
+	}
+
+}
